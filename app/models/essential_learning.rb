bb1e2f153c10b73fd850625b5940b3571628935e
Ben <baschenbrenner@hotmail.com>
2017-12-27 18:11:42 +0000

    added models

---
+++ app/models/essential_learning.rb
@@ -0,0 +1,2 @@
+class EssentialLearning < ApplicationRecord
+end
--- db/migrate/20171227180626_create_teachers.rb
+++ db/migrate/20171227180626_create_teachers.rb
@@ -1,13 +1,16 @@
-class Students < ActiveRecord::Migration[5.1]
+class CreateTeachers < ActiveRecord::Migration[5.1]
   def change
-    create_table :students do |t|
+    create_table :teachers do |t|
       t.string :first_name
       t.string :last_name
       t.string :preferred_name
       t.string :email
       t.string :password_digest
       t.string :image
+      t.string :quote
+      t.string :bio
       t.string :uid
+      
     end
   end
 end
