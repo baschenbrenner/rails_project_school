c40a51370bb44dda551d9793e1b248a6a2f91297
Ben <baschenbrenner@hotmail.com>
2017-12-28 17:06:08 +0000

    adding login feature

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,14 @@ Rails.application.routes.draw do
   resources :questions, only: [:create]
   resources :students, only: [:show, :index]
   resources :teachers, only: [:show, :index]
-
+  resources :sessions, only: [:new, :create, :destroy]
   resources :courses, only: [:show, :index]
   root 'welcome#home', as: 'home'
+  
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
   
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
