41bb940d50eb431963d9d1c65518dea4b81f783f
Ben <baschenbrenner@hotmail.com>
2017-12-28 17:30:56 +0000

    login working

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,14 +16,14 @@ class SessionsController < ApplicationController
   private
   
   def user_authenticate
-        if user_type == "student"
+        if params[:user_type] == "student"
             @student = Student.find_by(email: params[:email])
             return head(:forbidden) unless @student.authenticate(params[:password])
             session[:user_id] = @student.id 
         else
             @teacher = Teacher.find_by(email: params[:email])
-            return head(:forbidden) unless @student.authenticate(params[:password])
-            session[:user_id] = teacher.id 
+            return head(:forbidden) unless @teacher.authenticate(params[:password])
+            session[:user_id] = @teacher.id 
         end
   end
     
