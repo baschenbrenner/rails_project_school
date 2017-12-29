278d52f8c6af2344bd3abf37f511ec880c60467d
Ben <baschenbrenner@hotmail.com>
2017-12-29 16:48:27 +0000

    added nested routes and links

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :questions, only: [:create]
   resources :students, only: [:show, :index, :create, :update]
   resources :teachers, only: [:show, :index, :create, :update] do
-    resources :courses, only: [:show, :index, :create, :edit, :update]
+    resources :courses, only: [:show, :new, :index, :create, :edit, :update]
   end
   resources :sessions, only: [:new, :create, :destroy]
   resources :courses, only: [:show, :index]
