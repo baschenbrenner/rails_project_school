d6399fbb1d2b0b66e09d1d514199fbb1ee3e5cf0
baschenbrenner <baschenbrenner@hotmail.com>
2018-01-22 17:10:39 -0600

    added javascript file and working on JS functions

---
+++ app/assets/javascripts/courses.js
@@ -0,0 +1,26 @@
+$(document).ready(function() {
+   attachListeners();
+
+});
+
+
+function attachListeners() {
+  //
+  // $('td').on("click",function(){
+  //   doTurn(this)
+  // })
+
+  $('button#getCourses').on("click",() => showCourses());
+
+}
+
+function showCourses() {
+
+    $.get('/games').done(function(data) {
+      // let gamesLinks = $("#games")[0].innerHTML;
+      let games = data["data"];
+      if (games.length > 0)
+      {$("#games")[0].innerHTML = "";
+      games.forEach(function(element) {$("#games")[0].innerHTML += `<button id="game-${element["id"]}">Game ${element["id"]}</button></br>`})};
+      $('button[id^="game"]').on("click", function() {showGame(this)});
+}
